1992e9f1adcbc3eeacc771906997cc62ca8b7fa5
DarkWeirdo <sonic_unleashed@live.com>
2024-06-01 20:13:11 +0300

    Task 3 not fixed

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    var amenities = {};
+
+    $('input[type="checkbox"]').change(function() {
+        var id = $(this).data('data-id');
+        var name = $(this).data('data-name');
+
+        if($(this).is(':checked')) {
+            amenities[id] = name;
+        } else {
+            delete amenities[id];
+        }
+
+        var amenityList = Object.values(amenities).join(', ');
+        $('h4.amenities').text(amenityList);
+    });
+});
+
+$(document).ready(function() {
+    var apiStatusDiv = $('div#api_status');
+
+    $.get('http://0.0.0.0:5001/api/v1/status/')
+        .done(function(data) {
+            if (data.status === 'OK') {
+                apiStatusDiv.addClass('available');
+            } else {
+                apiStatusDiv.removeClass('available');
+            }
+        })
+        .fail(function() {
+            apiStatusDiv.removeClass('available');
+        });
+});
